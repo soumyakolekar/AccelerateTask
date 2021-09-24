1181c3954f0b744d109f294f0ed0151419d8e3f3
Soumya Kolekar <86036702+soumyakolekar@users.noreply.github.com>
2021-09-24 06:02:51 +0530

    Add files via upload

---
+++ TestwithoutSendKeyType.java
@@ -0,0 +1,27 @@
+package Test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestwithoutSendKeyType {
+  @Test
+  public void f() throws InterruptedException {
+	  WebDriver driver;
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\SOUMYA\\Downloads\\selenium-java-3.141.59\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&followup=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+	
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+	      js.executeScript ("document.getElementById('identifierId').value='Some_Random_Mail@gmail.com'");
+		Thread.sleep(2000);
+		
+		driver.close();
+		driver.quit();
+  }
+}
