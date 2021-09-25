388c5413273e7c53e9ec43d4d9cd56e3ad8459eb
Soumya Kolekar <86036702+soumyakolekar@users.noreply.github.com>
2021-09-25 11:50:23 +0530

    Add files via upload

--- Google.java
+++ Google.java
@@ -2,11 +2,14 @@ package Test1;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Google {
@@ -14,11 +17,12 @@ public class Google {
 	WebDriver driver;
 	@Before
 	public void setUp() throws Exception {
-		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\SOUMYA\\Downloads\\selenium-java-3.141.59\\chromedriver_win32\\chromedriver.exe");
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
+		  System.out.println("Start test");
+    	  System.setProperty("webdriver.chrome.driver","C:\\Users\\SOUMYA\\Downloads\\selenium-java-3.141.59\\chromedriver_win32\\chromedriver.exe");
+    	  driver = new ChromeDriver();
+    	  String url = "https://www.google.com";
+    	  driver.get(url);
+    	  driver.manage().window().maximize();
 	}
 
 	@After
@@ -29,9 +33,19 @@ public class Google {
 
 	@Test
 	public void test() throws InterruptedException {
-	driver.findElement(By.xpath("//input[@type='text']"));
-	//WebDriver driver = DriverFactory.getWebDriver();
-	//List<WebElement>tableList = driver.findElements(By.xpath()));
+		
+	driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("testing");
+	Thread.sleep(2000);
+	List<WebElement> list=driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='wM6W7d']"));
+	Thread.sleep(2000);
+	System.out.println("Total suggestion"+list.size());
+	Thread.sleep(2000);
+	for(int i=0;i<list.size();i++) {
+		System.out.println(list.get(i).getText());
+	if(list.get(i).getText().contains("testing types")) {
+		list.get(i).click();
+		break;
+	}}
 	}
 
 }
